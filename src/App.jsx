1f7af9af643a842bba2b2af7e274fb64d4d00fe7
Aleenasasi <aleenat511@gmail.com>
2024-12-26 19:40:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+
+
+
+
+import './App.css'
+
+function App() {
+  const handleClickFunction = (e) => {
+    const body = document.querySelector("body")
+    body.style.background = getRandomColor()
+
+  }
+
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+
+    }
+    return color
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>React Background Color Changer</h1>
+        <button className='btn btn-success
+        ' onClick={handleClickFunction}>Click here</button>
+      </div>
+    </>
+  )
+}
+
+export default App
+
